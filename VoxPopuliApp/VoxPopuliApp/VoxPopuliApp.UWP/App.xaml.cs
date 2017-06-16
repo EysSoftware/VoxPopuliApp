61f6e52d3f0301f69f9f5c0b5601f8e3591c5d4a
Arturo Rocha <arocha@equiposysistemas.net>
2017-06-16 13:37:57 -0700

    Qr open App only witth UWP

--- VoxPopuliApp/VoxPopuliApp/VoxPopuliApp.UWP/App.xaml.cs
+++ VoxPopuliApp/VoxPopuliApp/VoxPopuliApp.UWP/App.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using VoxPopuliApp.Views;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
 using Windows.Foundation;
@@ -67,6 +68,57 @@ namespace VoxPopuliApp.UWP
                 // configuring the new page by passing required information as a navigation
                 // parameter
                 rootFrame.Navigate(typeof(MainPage), e.Arguments);
+            }
+            // Ensure the current window is active
+            Window.Current.Activate();
+        }
+
+        protected override void OnActivated(IActivatedEventArgs e)
+        {
+            Frame rootFrame = Window.Current.Content as Frame;
+
+            if (rootFrame == null)
+            {
+                // Create a Frame to act as the navigation context and navigate to the first page
+                rootFrame = new Frame();
+
+                rootFrame.NavigationFailed += OnNavigationFailed;
+
+                Xamarin.Forms.Forms.Init(e);
+
+                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
+                {
+                    //TODO: Load state from previously suspended application
+                }
+
+                // Place the frame in the current Window
+                Window.Current.Content = rootFrame;
+            }
+
+            Type deepLinkPageType = typeof(MainPage);
+            if(e.Kind == ActivationKind.Protocol)
+            {
+                var protocolArgs = (ProtocolActivatedEventArgs)e;
+                switch (protocolArgs.Uri.AbsolutePath)
+                {
+                    case "/":
+                        break;
+                    case "/api/Campanias/":
+                        break;
+                    case "/api/Campanias/1":
+                        break;
+                    case "/voxpopuli/api/Campanias/1":
+                        deepLinkPageType = typeof(ItemDetailPage);
+                        break;
+                }
+            }
+
+            if (rootFrame.Content == null)
+            {
+                // When the navigation stack isn't restored navigate to the first page,
+                // configuring the new page by passing required information as a navigation
+                // parameter
+                rootFrame.Navigate(deepLinkPageType, e);
             }
             // Ensure the current window is active
             Window.Current.Activate();
